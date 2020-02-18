9d6eb46ab51a8db8272f21de396294538fb75a1e
Joao Duarte <joaoduarte227@gmail.com>
2020-02-18 16:10:26 +0000

    Add project files.

---
+++ ipodmusica/Desliga.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ipodmusica
+{
+    class Desliga
+    {
+        public bool ligar = true;
+        public bool desligar = false;
+    }
+
+}
