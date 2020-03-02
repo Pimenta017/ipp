04d048aa4431a9ef73ea1e083265d079adadf365
Pimenta017 <49915613+Pimenta017@users.noreply.github.com>
2020-03-02 16:10:36 +0000

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,222 @@
+﻿namespace ipodmusica
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button4 = new System.Windows.Forms.Button();
+            this.pictureBox6 = new System.Windows.Forms.PictureBox();
+            this.pictureBox5 = new System.Windows.Forms.PictureBox();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.button1.FlatAppearance.BorderSize = 0;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Location = new System.Drawing.Point(294, 317);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(58, 49);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Menu";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.button2.FlatAppearance.BorderSize = 0;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.Location = new System.Drawing.Point(357, 377);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(54, 41);
+            this.button2.TabIndex = 7;
+            this.button2.Text = ">>I";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.button3.FlatAppearance.BorderSize = 0;
+            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button3.Location = new System.Drawing.Point(236, 377);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(54, 41);
+            this.button3.TabIndex = 8;
+            this.button3.Text = "I<<";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(661, 76);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(16, 17);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "0";
+            this.label1.Visible = false;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(661, 117);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(16, 17);
+            this.label2.TabIndex = 11;
+            this.label2.Text = "0";
+            this.label2.Visible = false;
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.Color.White;
+            this.button4.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.button4.FlatAppearance.BorderSize = 0;
+            this.button4.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button4.Location = new System.Drawing.Point(294, 431);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(58, 49);
+            this.button4.TabIndex = 13;
+            this.button4.Text = "Ajuda";
+            this.button4.UseVisualStyleBackColor = false;
+            this.button4.Click += new System.EventHandler(this.button4_Click_2);
+            // 
+            // pictureBox6
+            // 
+            this.pictureBox6.Image = global::ipodmusica.Properties.Resources.Drake;
+            this.pictureBox6.Location = new System.Drawing.Point(201, 91);
+            this.pictureBox6.Name = "pictureBox6";
+            this.pictureBox6.Size = new System.Drawing.Size(245, 174);
+            this.pictureBox6.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox6.TabIndex = 5;
+            this.pictureBox6.TabStop = false;
+            this.pictureBox6.Visible = false;
+            // 
+            // pictureBox5
+            // 
+            this.pictureBox5.Image = global::ipodmusica.Properties.Resources.xxxtentacion;
+            this.pictureBox5.Location = new System.Drawing.Point(201, 91);
+            this.pictureBox5.Name = "pictureBox5";
+            this.pictureBox5.Size = new System.Drawing.Size(245, 174);
+            this.pictureBox5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox5.TabIndex = 4;
+            this.pictureBox5.TabStop = false;
+            this.pictureBox5.Visible = false;
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.Image = global::ipodmusica.Properties.Resources.trippie_redd1;
+            this.pictureBox4.Location = new System.Drawing.Point(201, 91);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(245, 174);
+            this.pictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox4.TabIndex = 3;
+            this.pictureBox4.TabStop = false;
+            this.pictureBox4.Visible = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::ipodmusica.Properties.Resources.apple;
+            this.pictureBox1.Location = new System.Drawing.Point(201, 91);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(245, 174);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Visible = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = global::ipodmusica.Properties.Resources.Ipod_123;
+            this.pictureBox2.Location = new System.Drawing.Point(107, 12);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(453, 570);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            this.pictureBox2.Click += new System.EventHandler(this.pictureBox2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(860, 594);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox6);
+            this.Controls.Add(this.pictureBox5);
+            this.Controls.Add(this.pictureBox4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.pictureBox2);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "we";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.PictureBox pictureBox5;
+        private System.Windows.Forms.PictureBox pictureBox6;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button4;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,292 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Media;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ipodmusica
+{
+    public partial class Form1 : Form
+    {
+        private int num1 = 0;
+        private int num2 = 0;
+        private int resultado;
+        bool on = true;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            pictureBox1.Visible = true;
+            BringToFront();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            num1 = Convert.ToInt32(label1.Text);
+            resultado = num1 + 1;
+            label1.Text = Convert.ToString(resultado);
+
+            int lbl_num = Convert.ToInt32(resultado);
+            SoundPlayer Som1 = new SoundPlayer("C:\\Users\\prive\\source\\repos\\ipodmusica\\ipodmusica\\Resources\\Trippie_Redd.wav");
+            SoundPlayer Som2 = new SoundPlayer("C:\\Users\\prive\\source\\repos\\ipodmusica\\ipodmusica\\Resources\\xxxtentacion.wav");
+            SoundPlayer Som3 = new SoundPlayer("C:\\Users\\prive\\source\\repos\\ipodmusica\\ipodmusica\\Resources\\drake.wav");
+            switch (lbl_num)
+            {
+                case 1:
+                    pictureBox4.Visible = true;
+                    pictureBox4.BringToFront();
+                    Som1.Play();
+                    break;
+                case 2:
+                    pictureBox4.Visible = false;
+                    pictureBox5.BringToFront();
+                    pictureBox5.Visible = true;
+                    Som2.Play();
+                    break;
+                case 3:
+                    pictureBox5.Visible = false;
+                    pictureBox6.BringToFront();
+                    pictureBox6.Visible = true;
+                    Som3.Play();
+                    break;
+                case 4:
+                    pictureBox4.Visible = true;
+                    pictureBox4.BringToFront();
+                    Som1.Play();
+                    break;
+                case 5:
+                    pictureBox4.Visible = false;
+                    pictureBox5.BringToFront();
+                    pictureBox5.Visible = true;
+                    Som2.Play();
+                    break;
+                case 6:
+                    pictureBox5.Visible = false;
+                    pictureBox6.BringToFront();
+                    pictureBox6.Visible = true;
+                    Som3.Play();
+                    break;
+                case 7:
+                    pictureBox4.Visible = true;
+                    pictureBox4.BringToFront();
+                    Som1.Play();
+                    break;
+                case 8:
+                    pictureBox4.Visible = false;
+                    pictureBox5.BringToFront();
+                    pictureBox5.Visible = true;
+                    Som2.Play();
+                    break;
+                case 9:
+                    pictureBox5.Visible = false;
+                    pictureBox6.BringToFront();
+                    pictureBox6.Visible = true;
+                    Som3.Play();
+                    break;
+                case 10:
+                    pictureBox4.Visible = true;
+                    pictureBox4.BringToFront();
+                    Som1.Play();
+                    break;
+                case 11:
+                    pictureBox4.Visible = false;
+                    pictureBox5.BringToFront();
+                    pictureBox5.Visible = true;
+                    Som2.Play();
+                    break;
+                case 12:
+                    pictureBox5.Visible = false;
+                    pictureBox6.BringToFront();
+                    pictureBox6.Visible = true;
+                    Som3.Play();
+                    break;
+
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            num1 = Convert.ToInt32(label1.Text);
+            resultado = num1 + 1;
+            label1.Text = Convert.ToString(resultado);
+
+            int lbl_num = Convert.ToInt32(resultado);
+            SoundPlayer Som1 = new SoundPlayer("C:\\Users\\prive\\source\\repos\\ipodmusica\\ipodmusica\\Resources\\Trippie_Redd.wav");
+            SoundPlayer Som2 = new SoundPlayer("C:\\Users\\prive\\source\\repos\\ipodmusica\\ipodmusica\\Resources\\xxxtentacion.wav");
+            SoundPlayer Som3 = new SoundPlayer("C:\\Users\\prive\\source\\repos\\ipodmusica\\ipodmusica\\Resources\\drake.wav");
+            switch (lbl_num)
+            {
+                case 1:
+                    pictureBox4.Visible = true;
+                    pictureBox4.BringToFront();
+                    Som1.Play();
+                    break;
+                case 2:
+                    pictureBox4.Visible = false;
+                    pictureBox5.BringToFront();
+                    pictureBox5.Visible = true;
+                    Som2.Play();
+                    break;
+                case 3:
+                    pictureBox5.Visible = false;
+                    pictureBox6.BringToFront();
+                    pictureBox6.Visible = true;
+                    Som3.Play();
+                    break;
+                case 4:
+                    pictureBox4.Visible = true;
+                    pictureBox4.BringToFront();
+                    Som1.Play();
+                    break;
+                case 5:
+                    pictureBox4.Visible = false;
+                    pictureBox5.BringToFront();
+                    pictureBox5.Visible = true;
+                    Som2.Play();
+                    break;
+                case 6:
+                    pictureBox5.Visible = false;
+                    pictureBox6.BringToFront();
+                    pictureBox6.Visible = true;
+                    Som3.Play();
+                    break;
+                case 7:
+                    pictureBox4.Visible = true;
+                    pictureBox4.BringToFront();
+                    Som1.Play();
+                    break;
+                case 8:
+                    pictureBox4.Visible = false;
+                    pictureBox5.BringToFront();
+                    pictureBox5.Visible = true;
+                    Som2.Play();
+                    break;
+                case 9:
+                    pictureBox5.Visible = false;
+                    pictureBox6.BringToFront();
+                    pictureBox6.Visible = true;
+                    Som3.Play();
+                    break;
+                case 10:
+                    pictureBox4.Visible = true;
+                    pictureBox4.BringToFront();
+                    Som1.Play();
+                    break;
+                case 11:
+                    pictureBox4.Visible = false;
+                    pictureBox5.BringToFront();
+                    pictureBox5.Visible = true;
+                    Som2.Play();
+                    break;
+                case 12:
+                    pictureBox5.Visible = false;
+                    pictureBox6.BringToFront();
+                    pictureBox6.Visible = true;
+                    Som3.Play();
+                    break;
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            num1 = Convert.ToInt32(label1.Text);
+            resultado = num1 + 1;
+            label1.Text = Convert.ToString(resultado);
+
+            int lbl_num = Convert.ToInt32(resultado);
+            SoundPlayer Som1 = new SoundPlayer("C:\\Users\\prive\\source\\repos\\ipodmusica\\ipodmusica\\Resources\\Trippie Redd.wav");
+            SoundPlayer Som2 = new SoundPlayer("C:\\Users\\prive\\source\\repos\\ipodmusica\\ipodmusica\\Resources\\xxxtentacion.wav");
+            SoundPlayer Som3 = new SoundPlayer("C:\\Users\\prive\\source\\repos\\ipodmusica\\ipodmusica\\Resources\\drake.wav");
+            switch (lbl_num)
+            {
+                case 1:
+                    pictureBox4.Visible = true;
+                    pictureBox4.BringToFront();
+                    Som1.Play();
+                    break;
+                case 2:
+                    pictureBox4.Visible = false;
+                    pictureBox5.BringToFront();
+                    pictureBox5.Visible = true;
+                    Som2.Play();
+                    break;
+                case 3:
+                 pictureBox5.Visible = false;
+                    pictureBox6.BringToFront();
+                    pictureBox6.Visible = true;
+                    Som3.Play();
+                    break;
+                case 4:
+                    pictureBox4.Visible = true;
+                    pictureBox4.BringToFront();
+                    Som1.Play();
+                    break;
+                case 5:
+                    pictureBox4.Visible = false;
+                    pictureBox5.BringToFront();
+                    pictureBox5.Visible = true;
+                    Som2.Play();
+                    break;
+                case 6:
+                    pictureBox5.Visible = false;
+                    pictureBox6.BringToFront();
+                    pictureBox6.Visible = true;
+                    Som3.Play();
+                    break;
+                case 7:
+                    pictureBox4.Visible = true;
+                    pictureBox4.BringToFront();
+                    Som1.Play();
+                    break;
+                case 8:
+                    pictureBox4.Visible = false;
+                    pictureBox5.BringToFront();
+                    pictureBox5.Visible = true;
+                    Som2.Play();
+                    break;
+                case 9:
+                    pictureBox5.Visible = false;
+                    pictureBox6.BringToFront();
+                    pictureBox6.Visible = true;
+                    Som3.Play();
+                    break;
+                case 10:
+                    pictureBox4.Visible = true;
+                    pictureBox4.BringToFront();
+                    Som1.Play();
+                    break;
+                case 11:
+                    pictureBox4.Visible = false;
+                    pictureBox5.BringToFront();
+                    pictureBox5.Visible = true;
+                    Som2.Play();
+                    break;
+                case 12:
+                    pictureBox5.Visible = false;
+                    pictureBox6.BringToFront();
+                    pictureBox6.Visible = true;
+                    Som3.Play();
+                    break;
+                }
+            }
+
+        private void pictureBox2_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void button4_Click_2(object sender, EventArgs e)
+        {
+            Help.ShowHelp(this, "C:\\Users\\prive\\source\\repos\\ipodmusica\\ipodmusica\\Resources\\ajuda.chm");
+        }
+    }
+    }
+
+
